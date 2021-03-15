b344a7f52ac8d618541f267cac697ddb1466a702
Kevin Botero <kbotero201@gmail.com>
2021-03-15 17:05:28 -0400

    code cleanup

--- src/SortAlgorithims/mergeSort.js
+++ src/SortAlgorithims/mergeSort.js
@@ -2,11 +2,11 @@
 function mergeSort(arr){
 
         if(arr.length <= 1) return arr
-        let mid = Math.floor(arr.length/2)
-        let left = mergeSort(arr.slice(0, mid))
-        let right = mergeSort(arr.slice(mid))
+        const mid = Math.floor(arr.length/2)
+        const left = mergeSort(arr.slice(0, mid))
+        const right = mergeSort(arr.slice(mid))
         
-        let results = []
+        const results = []
         let i = 0
         let j = 0
     
